02750b0120f3852f4b1e3859336a6d23a1af2ce1
Mooneous <4087022@naver.com>
2022-08-24 14:57:31 +0900

    DB로부터 글 데이터를 list컴포넌트에 출력

--- react/src/community/List.js
+++ react/src/community/List.js
@@ -1,7 +1,33 @@
 import Layout from '../common/Layout';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 
 function List() {
-	return <Layout name={'List'}>List</Layout>;
+	const [List, setList] = useState([]);
+
+	useEffect(() => {
+		axios
+			.post('/api/read')
+			.then((res) => {
+				if (res.data.success) {
+					console.log(res.data.communityList);
+					setList(res.data.communityList);
+				}
+			})
+			.catch((err) => console.log(err));
+	}, []);
+
+	return (
+		<Layout name={'List'}>
+			{List.map((post) => {
+				return (
+					<article key={post._id}>
+						<h2>{post.title}</h2>
+					</article>
+				);
+			})}
+		</Layout>
+	);
 }
 
 export default List;
