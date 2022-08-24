bf21026954c030d0806264a12b16ba7580858286
Mooneous <4087022@naver.com>
2022-08-24 14:52:53 +0900

    글저장 완료시 강제로 목록화면으로 이동

--- react/src/community/Create.js
+++ react/src/community/Create.js
@@ -1,18 +1,28 @@
 import Layout from '../common/Layout';
 import axios from 'axios';
 import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 function Create() {
+	const navigate = useNavigate();
 	const [Tit, setTit] = useState('');
 	const [Con, setCon] = useState('');
 
 	const handleCreate = () => {
+		if (Tit.trim() === '' || Con.trim() === '') return alert('제목과 본문을 모두 입력하세요.');
+
 		const item = { title: Tit, content: Con };
 
 		axios
 			.post('/api/create', item)
 			.then((res) => {
 				console.log(res);
+				if (res.data.success) {
+					alert('글 저장이 완료되었습니다.');
+					navigate('/list');
+				} else {
+					alert('글 저장에 실패했습니다.');
+				}
 			})
 			.catch((err) => console.log(err));
 	};
