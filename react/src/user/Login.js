3adf8983567f38c831abbeb6f76e5092b404275f
Mooneous <4087022@naver.com>
2022-08-25 15:38:31 +0900

    로그인컴포넌트에 로그인처리

--- react/src/user/Login.js
+++ react/src/user/Login.js
@@ -1,7 +1,56 @@
 import Layout from '../common/Layout';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import firebase from '../firebase';
+import styled from 'styled-components';
+
+const BtnSet = styled.div`
+	margin-top: 20px;
+`;
 
 function Login() {
-	return <Layout name={'Login'}>Login</Layout>;
+	const navigate = useNavigate();
+	const [Email, setEmail] = useState('');
+	const [Pwd, setPwd] = useState('');
+	const [Err, setErr] = useState('');
+
+	const handleLogin = async () => {
+		if (!(Email && Pwd)) return alert('모든값을 입력하세요.');
+
+		try {
+			await firebase.auth().signInWithEmailAndPassword(Email, Pwd);
+			navigate('/');
+		} catch (err) {
+			console.log(err.code);
+
+			if (err.code === 'auth/user-not-found') setErr('존재하지 않는 이메일입니다.');
+			else if (err.code === 'auth/wrong-password') setErr('비밀번호 정보가 일치하지 않습니다.');
+			else setErr('로그인에 실패했습니다.');
+		}
+	};
+
+	return (
+		<Layout name={'Login'}>
+			<input
+				type='email'
+				value={Email}
+				placeholder='이메일주소를 입력하세요.'
+				onChange={(e) => setEmail(e.target.value)}
+			/>
+			<input
+				type='password'
+				value={Pwd}
+				placeholder='비밀번호를 입력하세요'
+				onChange={(e) => setPwd(e.target.value)}
+			/>
+
+			<BtnSet>
+				<button onClick={handleLogin}>LOGIN</button>
+				<button onClick={() => navigate('/join')}>JOIN</button>
+			</BtnSet>
+			{Err !== '' && <p>{Err}</p>}
+		</Layout>
+	);
 }
 
 export default Login;
