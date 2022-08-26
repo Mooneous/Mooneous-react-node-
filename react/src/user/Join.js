b6ecfd1a0e9a883521f8e4e18bbc4891862710fe
Mooneous <4087022@naver.com>
2022-08-26 12:00:00 +0900

    회원가입시 firebase로 받아진 유저 정보값을 DB에도 저장 및 회원가입시 자동 로그아웃

--- react/src/user/Join.js
+++ react/src/user/Join.js
@@ -3,6 +3,7 @@ import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
 import firebase from '../firebase';
+import axios from 'axios';
 
 const BtnSet = styled.div`
 	margin-top: 20px;
@@ -18,11 +19,31 @@ function Join() {
 	const handleJoin = async () => {
 		if (!(Name && Email && Pwd1 && Pwd2)) return alert('모든 양식을 입력하세요.');
 		if (Pwd1 !== Pwd2) return alert('비밀번호 2개를 동일하게 입력하세요.');
+		if (Pwd1.length < 6) return alert('비밀번호는 최소 6글자 이상 입력하세요.');
 
 		let createdUser = await firebase.auth().createUserWithEmailAndPassword(Email, Pwd1);
 		await createdUser.user.updateProfile({ displayName: Name });
+
 		console.log(createdUser.user);
 		navigate('/login');
+
+		//firebase로부터 인증정보값이 넘어오면 해당 정보값을 다시 객체에 옮겨담기
+		const item = {
+			email: createdUser.user.multiFactor.user.email,
+			displayName: createdUser.user.multiFactor.user.displayName,
+			uid: createdUser.user.multiFactor.user.uid,
+		};
+
+		//서버쪽에 post요청 보내기
+		axios.post('/api/user/join', item).then((res) => {
+			if (res.data.success) {
+				firebase.auth().signOut();
+				alert('회원가입이 완료되었습니다.');
+				navigate('/login');
+			} else {
+				return alert('회원가입에 실패했습니다.');
+			}
+		});
 	};
 
 	return (
